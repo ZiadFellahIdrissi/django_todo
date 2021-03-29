28bda3477a57fae978fd5fd0c1e6894e0cbd9374
Aknakaye <ahmedreda.aknakaye123@gmail.com>
2021-03-29 13:36:13 +0100

    update etudiant

--- first/urls.py
+++ first/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('ajax/affiche_tableau_etudiant/', views.affiche_tableau_etudiant, name="ajouter_etudiant0"),
     path('ajax/ajouter_etudiant0/', views.ajouter_etudinat, name="ajouter_etudiant0"),
     path('ajax/suprimer_etudiant/',views.supprimer_etudiant),
+    path('ajax/modifier_etudiant/',views.modifier_etudiant)
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -17,6 +17,19 @@ def supprimer_etudiant(request):
         Etudiant.objects.filter(id=v_id).delete()
     return HttpResponse()
 
+def modifier_etudiant(request):
+    if request.method == 'POST':
+        v_id =  request.POST['id'] 
+        v_nom =  request.POST['nom'] 
+        v_prenom =  request.POST['prenom']
+        v_age =  request.POST['age']
+        Etudiant.objects.filter(id=v_id).update(
+            nom = v_nom,
+            prenom = v_prenom,
+            age_1 = v_age
+        )
+    return HttpResponse()
+
 def ajouter_etudinat(request):
     if request.method == 'POST':
         v_nom =  request.POST['nom'] 
